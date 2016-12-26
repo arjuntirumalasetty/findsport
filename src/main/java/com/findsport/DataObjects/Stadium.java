c816904da404f2ffd0f5a94c99780e34a8cc56e8
arjun <arjuntirumalasetty@gmail.com>
2016-12-26 20:00:27 +0530

    created data object for stadium

---
+++ src/main/java/com/findsport/DataObjects/Stadium.java
@@ -0,0 +1,38 @@
+package com.findsport.DataObjects;
+
+/**
+ * Created by Arjun on 12/26/2016.
+ */
+public class Stadium {
+
+    private String stadiumID;
+    private String stadiumName;
+    private String stadiumLongitude;
+    private String stadiumLatitude;
+    private String stadiumPhoneNo;
+    private String stadiumAddress;
+
+    public String getStadiumID() {
+        return stadiumID;
+    }
+
+    public String getStadiumName() {
+        return stadiumName;
+    }
+
+    public String getStadiumLongitude() {
+        return stadiumLongitude;
+    }
+
+    public String getStadiumLatitude() {
+        return stadiumLatitude;
+    }
+
+    public String getStadiumPhoneNo() {
+        return stadiumPhoneNo;
+    }
+
+    public String getStadiumAddress() {
+        return stadiumAddress;
+    }
+}
