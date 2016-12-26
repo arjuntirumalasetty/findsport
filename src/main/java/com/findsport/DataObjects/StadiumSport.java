4d3a4c40c41ca353a72bd163bbc131b158762e47
arjun <arjuntirumalasetty@gmail.com>
2016-12-26 21:53:56 +0530

    updated sql file

---
+++ src/main/java/com/findsport/DataObjects/StadiumSport.java
@@ -0,0 +1,12 @@
+package com.findsport.DataObjects;
+
+/**
+ * Created by Arjun on 12/26/2016.
+ */
+public class StadiumSport {
+
+    private String stadiumID;
+    private String sportID;
+    private String sportName;
+    pr
+}
--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -1,22 +1,29 @@
 CREATE TABLE findsport.stadium_details (
-    stadium_id VARCHAR(10),
+    stadium_id INT NOT NULL AUTO_INCREMENT,
     stadium_name VARCHAR(20),
     stadium_longitude FLOAT(10 , 6 ),
     stadium_latitide FLOAT(10 , 6 ),
     stadium_phoneno VARCHAR(10),
-    stadium_adress VARCHAR(40)
+    stadium_adress VARCHAR(40),
+    PRIMARY KEY (stadium_id)
 );
 
 CREATE TABLE findsport.stadium_sport (
-    stadium_id VARCHAR(10),
-    sport_id VARCHAR(10),
+    stadium_id INT NOT NULL,
+    sport_id INT(2),
+    court_count INT(2),
+    PRIMARY KEY (stadium_id , sport_id)
+);
+
+CREATE TABLE findsport.sport_details (
+    sport_id INT(2),
     sport_name VARCHAR(20),
-    court_id VARCHAR(10)
+    PRIMARY KEY (sport_id)
 );
 
 CREATE TABLE findsport.court_availability (
-    stadium_id VARCHAR(10),
-    court_id VARCHAR(10),
+    stadium_id LONG NOT NULL,
+    court_id INT(2),
     booked_date DATE,
     booked_from TIME,
     booked_till TIME
