40dfe068b0cb0039438081cc372b05a127873175
pavan <kpavan.kumar36@gmail.com>
2016-12-27 13:05:30 +0530

    added stadium mock object class

---
+++ src/test/java/com/findsport/mockObjects/MockObjectCreator.java
@@ -0,0 +1,18 @@
+package com.findsport.mockObjects;
+
+import com.findsport.DataObjects.Stadium;
+
+public class MockObjectCreator {
+	
+	public static Stadium getMockStadiumObject() {
+		Stadium mockStadium = new Stadium();
+		mockStadium.setStadiumAddress("testStadiumaddress");
+		mockStadium.setStadiumID(1234);
+		mockStadium.setStadiumLatitude("1234.123");
+		mockStadium.setStadiumLongitude("1234.1234");
+		mockStadium.setStadiumName("testStadiumName");
+		mockStadium.setStadiumPhoneNo("1234567890");	
+		return mockStadium;
+	}
+
+}
