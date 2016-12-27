a3b991e79850a6bca0ea8a59916b37fc705a89b9
pavan <kpavan.kumar36@gmail.com>
2016-12-27 18:17:58 +0530

    reading from query.properties

--- src/main/java/com/findsport/DataAccessor/StadiumDAO.java
+++ src/main/java/com/findsport/DataAccessor/StadiumDAO.java
@@ -3,11 +3,7 @@ package com.findsport.DataAccessor;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import com.findsport.DataObjects.Stadium;
 import com.findsport.Main.StartService;
 import com.findsport.mockObjects.MockObjectCreator;
@@ -15,34 +11,31 @@ import com.findsport.mockObjects.MockObjectCreator;
 /**
  * Created by Arjun on 12/27/2016.
  */
-@PropertySource("classpath:query.properties")
 public class StadiumDAO {
-	
-	@Value("$(insert_stadium_details)")
-	private static String insert_stadium_details;
 
 	public static boolean insertData(Stadium stadiumDataObject) {
-		
+
 		try {
+			Properties prop = new Properties();
+			InputStream input = new FileInputStream("query.properties");
+			prop.load(input);
+			String insert_stadium_details = prop.getProperty("insert_stadium_details");
 			JdbcTemplate template = StartService.getJdbcTemplate();
 			int status = template.update(insert_stadium_details, stadiumDataObject.getStadiumID(), stadiumDataObject.getStadiumName(),
-					                                stadiumDataObject.getStadiumLongitude(), stadiumDataObject.getStadiumLatitude(),
-					                                stadiumDataObject.getStadiumPhoneNo(), stadiumDataObject.getStadiumAddress());
+					stadiumDataObject.getStadiumLongitude(), stadiumDataObject.getStadiumLatitude(),
+					stadiumDataObject.getStadiumPhoneNo(), stadiumDataObject.getStadiumAddress());
 			System.out.print("status : "+ status);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 			return false;
-			
 		}
 		return true;	
 	}
-	
+
 	public static void main (String[] args) {
 		Stadium stadiumDataObject = MockObjectCreator.getMockStadiumObject();
 		System.out.println(" success : " + insertData(stadiumDataObject));
-		
+
 	}
-	
-	
 }
